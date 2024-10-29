9ab8a82b6d2e2a86ab95ae99888480e08dd543c0
Damiano Palmisano <dpalmisano98@gmail.com>
2024-10-29 14:50:56 +0100

    added songs

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -2,6 +2,10 @@ extends RigidBody2D
 
 @onready var score = %ScoreLabel
 @onready var resume = %Resume
+@onready var scoreSong = %ScoreSong
+@onready var paddleSong = %PaddleSong
+@onready var wallSong = %WallSong
+
 var velocity = Vector2(400, 400)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,10 +17,26 @@ func _process(delta: float) -> void:
 func _on_player_border_body_entered(_body:Node2D) -> void:
 	score.player2Score += 1
 	resume.visible = true
+	scoreSong.play()
 	get_tree().paused = true
 
 
 func _on_player_2_border_body_entered(_body:Node2D) -> void:
 	score.playerScore += 1
 	resume.visible = true
+	scoreSong.play()
+	await scoreSong.finished
 	get_tree().paused = true
+
+func _on_area_2d_body_entered(body:Node2D) -> void:
+	if (body.name == "Ball"):
+		wallSong.play()
+
+func _on_area_2d_player_2_body_entered(body:Node2D) -> void:
+	if (body.name == "Ball"):
+		paddleSong.play()
+
+func _on_area_2d_player_body_entered(body:Node2D) -> void:
+	if (body.name == "Ball"):
+		paddleSong.play()
+
